adfd4726f0f6959db6326cb2ef45f44abc800ed5
Vaibhav Kumar K R <vaibhav.kumarkr@thoughtworks.com>
2026-08-27 19:18:01 +0530

    Update agent.py

--- agents/transportation_agent/agent.py
+++ agents/transportation_agent/agent.py
@@ -9,7 +9,7 @@ def create_transportation_agent(output_key: str = "transportation_result") -> Ag
         disallow_transfer_to_peers=True,
         name="transportation_agent",
         output_key=output_key,
-        description="""We are planning a trip to new zealand . What are the available modes of transportation and what is their cost ?What are famous places to visit in new zealand ? what is the required duration of days to cover all these must visit tourist spots ?what is the best time to travel to new zealand and what are the different available modes of local transport ? what is the best season to travel to new zealand ?what is the visa required to travel to new zealand as a tourist ? what are the required documents for those ?""",
+        description="""We are planning a trip to a country example new zealand here . What are the available modes of transportation and what is their cost ?What are famous places to visit  ? what is the required duration of days to cover all these must visit tourist spots ?what is the best time to travel there and what are the different available modes of local transport ? what is the best season to travel to that country ?what is the visa required to travel  as a tourist ? what are the required documents for those ?""",
         instruction="""...""",
     )
 
